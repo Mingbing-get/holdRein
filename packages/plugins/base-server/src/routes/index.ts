deb016f400f25f71a1c956e78d2755cad6465d27
明兵 <18512863641@163.com>
2026-06-17 09:40:34 +0800

    feat: 注释测试代码

--- packages/plugins/base-server/src/routes/index.ts
+++ packages/plugins/base-server/src/routes/index.ts
@@ -5,9 +5,9 @@ import type { ServerPlugin } from '@hold-rein/plugin-server'
 export default function createRouter(context: ServerPlugin.RouteContext): Router {
   const router = Router()
 
-  router.get('/test', (request, response) => {
-    context.sendSuccess(response, 'plugin ok')
-  })
+  // router.get('/test', (request, response) => {
+  //   context.sendSuccess(response, 'plugin ok')
+  // })
 
   return router
 }
