1f563695b7220aca41d9f2a5018afccd5adf3896
明兵 <18512863641@163.com>
2026-07-07 15:04:44 +0800

    feat: 初始化五子棋插件

---
+++ packages/plugins/gomoku/src/plugin-id.ts
@@ -0,0 +1,1 @@
+export const PLUGIN_ID = "__gomoku__plugin";
---
+++ packages/plugins/gomoku/src/web.ts
@@ -0,0 +1,9 @@
+import type { WebPlugin } from "@hold-rein/plugin-web";
+
+import { PLUGIN_ID } from "./plugin-id";
+
+const webPlugin: WebPlugin.Plugin = {
+  id: PLUGIN_ID
+};
+
+export default webPlugin;
